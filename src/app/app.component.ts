2a35061c3fc8aff3c5ab640af1576092109e3c7a
Carlos Mestas <carlos93esp@gmail.com>
2022-03-09 17:37:35 +0100

    Menu

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'pipesApp';
+  nombre: string = 'Carlos';
+  valor: number = 1000;
+
+  mostrarNombre(){
+    return this.nombre;
+  }
 }
